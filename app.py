3d832932bd1ad8efdd5c26809293c2d93969eb66
mcarrenoUE <mcarreno@uniempresarial.edu.co>
2025-03-24 10:51:50 -0500

    Fix: configuración de puerto para Railway

--- app.py
+++ app.py
@@ -30,3 +30,8 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))  # Este puerto lo exige Railway
+    app.run(host='0.0.0.0', port=port)
